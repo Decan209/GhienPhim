090b92aec47da90097ef8d48d62e3dbc6c1df14a
NgCan <118377905+Decan209@users.noreply.github.com>
2023-05-14 22:09:28 +0700

    test

--- server/src/index.js
+++ server/src/index.js
@@ -12,10 +12,11 @@ export const SECET_KEY = process.env.SECET_KEY;
 export const DOMAIN = process.env.DOMAIN;
 const app = express();
 dbConnect();
-app.use(cors({
-    credentials:true,
-    origin:['http://localhost:3000','https://ghienphim.onrender.com']
-}));
+// app.use(cors({
+//     credentials:true,
+//     origin:['http://localhost:3000','https://ghienphim.onrender.com']
+// }));
+app.use('*',cors())
 app.use(express.json());
 app.use(cookieParser());
 
